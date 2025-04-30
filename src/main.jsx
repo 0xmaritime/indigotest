a22228f4841661472ab204fd448c3bc8fda3f1d4
hulkhogan2024 <96657914+hulkhogan2024@users.noreply.github.com>
2025-04-30 16:06:35 +0300

    password protection removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import PasswordPopup from './components/PasswordPopup';
 import './main.css';
 
-function Root() {
-  const [authenticated, setAuthenticated] = useState(false);
-
-  useEffect(() => {
-    // Force password check by clearing auth state
-    localStorage.removeItem('isAuthenticated');
-    setAuthenticated(false);
-  }, []);
-
-  const handleSuccess = () => {
-    setAuthenticated(true);
-  };
-
-  return (
-    <React.StrictMode>
-      <BrowserRouter>
-        {authenticated ? (
-          <App />
-        ) : (
-          <PasswordPopup onSuccess={handleSuccess} />
-        )}
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
